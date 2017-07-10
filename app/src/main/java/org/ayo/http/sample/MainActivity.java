12e43f5e0d3513740f6d881eed26ddf5cd33a002
cowthan <cowthan@163.com>
2017-07-10 23:40:32 +0800

    d

--- app/src/main/java/org/ayo/http/sample/MainActivity.java
+++ app/src/main/java/org/ayo/http/sample/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.demo_http);
 
         getRequest().tag("测试接口")
                 .actionGet()
